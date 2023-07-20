63d2e2347a065c2d9f318e6e9524e7da2ab4d463
Xavier GARNAUD <garnaudxavier@gmail.com>
2023-07-20 17:56:27 +0200

    remove generalized eigenvalues from lapack

--- src/metric_reduction.rs
+++ src/metric_reduction.rs
@@ -1,45 +1,40 @@
-use crate::linalg::lapack_generalized_symmetric_eigenvalues;
 use nalgebra::allocator::Allocator;
-use nalgebra::{Const, DefaultAllocator, SMatrix, SVector};
+use nalgebra::{Const, DefaultAllocator, SMatrix};
 
 /// Compute the simulaneous reduction of two metrics.
-/// Let $`\mathcal P = (e_0 | ... | e_d)`$ be the generalized eigenvectors of $`(\mathcal M_0, \mathcal M_1)`$:
-/// ```math
-/// \mathcal M_0 \mathcal P = \Lambda \mathcal M_1 \mathcal P
-/// ```
-/// then
-/// ```math
-/// \mathcal M_i = \mathcal P^{-1, T} \Lambda^{(i)} \mathcal P^{(-1)}
-/// ```
-/// with $`\Lambda^{(i)}_{jk} = e_j^T \mathcal M_i e_k \delta_{jk}`$
-/// The intersection is then
-/// ```math
-/// \mathcal M_0 \cap \mathcal M_1 = \mathcal P^{-1, T} \Lambda^{(i,j)} \mathcal P^{(-1)}
-/// ```
-/// with $`\Lambda^{(i,j)}_{jk} = max(\Lambda^{(i)}_{jk}, \Lambda^{(j)}_{jk})`$.
-///
-/// NB: the generalized eigenvalue problem is solved using the `dsygv` function in Lapack
+/// - It is computed as follows:
+///   - Compute the EVD of $\mathcal M = \mathcal M_2^{-1/2} \mathcal M_1 \mathcal M_2^{-1/2} = P^T\Lambda P$
+///   - Compute $M = \mathcal M_2^{-1/2} P = (e_0 | ... | e_d)$
+///   - Compute $s_i = mav(e_i^T\mathcal M_1 e, e_i^T\mathcal M_2 e)$
+///   - $\mathcal M_{1 \cap 2} = \mathcal M_2^{1/2} M diag(s) M^T \mathcal M_2^{1/2}$
 pub fn simultaneous_reduction<const D: usize>(
-    mat_a: SMatrix<f64, D, D>,
-    mat_b: SMatrix<f64, D, D>,
-) -> SMatrix<f64, D, D> {
-    let mut p = mat_a;
-    let mut tmp = mat_b;
-    let mut work = [0.0; 9]; // 9 >= 3*D as D <= 3
+    a: SMatrix<f64, D, D>,
+    b: SMatrix<f64, D, D>,
+) -> SMatrix<f64, D, D>
+where
+    Const<D>: nalgebra::DimName + nalgebra::ToTypenum + nalgebra::DimSub<nalgebra::U1>,
+    DefaultAllocator: Allocator<f64, Const<D>>
+        + Allocator<f64, <Const<D> as nalgebra::DimSub<nalgebra::U1>>::Output>,
+{
+    let mut eig = b.symmetric_eigen();
+    eig.eigenvalues.iter_mut().for_each(|s| *s = 1.0 / s.sqrt());
+    let tmp = eig.recompose();
 
-    lapack_generalized_symmetric_eigenvalues::<D>(p.as_mut_slice(), tmp.as_mut_slice(), &mut work);
+    let p = tmp * a * tmp;
+    let p = p.symmetric_eigen().eigenvectors;
+    let tmp = tmp * p;
 
-    let mut s = SVector::<f64, D>::zeros();
-
-    for j in 0..D {
-        let v = p.column(j);
-        let sa = v.dot(&(mat_a * v));
-        let sb = v.dot(&(mat_b * v));
-        s[j] = f64::max(f64::abs(sa), f64::abs(sb));
+    let mut s = SMatrix::<f64, D, D>::zeros();
+    let (n, _) = a.shape();
+    for i in 0..n {
+        let v = tmp.column(i);
+        s[n * i + i] = f64::max(v.dot(&(a * v)), v.dot(&(b * v)));
     }
-    let s = SMatrix::<f64, D, D>::from_diagonal(&s);
-    let p = p.try_inverse().unwrap();
-    p.transpose() * s * p
+
+    eig.eigenvalues.iter_mut().for_each(|s| *s = 1.0 / *s);
+    let tmp = eig.recompose();
+    let tmp = p.transpose() * tmp;
+    tmp.transpose() * s * tmp
 }
 
 /// Bound the step between two metrics $`\mathcal M_1`$ and $`\mathcal M_2`$.
@@ -93,19 +88,20 @@ mod tests {
     use super::{control_step, simultaneous_reduction};
     use nalgebra::{SMatrix, SVector};
 
-    fn test_simred<const D: usize>() {
+    #[test]
+    fn test_simred_2d() {
         let eps = 1e-8;
 
         for _ in 0..100 {
-            let mat_r = SMatrix::<f64, D, D>::new_random();
+            let mat_r = SMatrix::<f64, 2, 2>::new_random();
             let mat_a = mat_r.transpose() * mat_r;
-            let mat_r = SMatrix::<f64, D, D>::new_random();
+            let mat_r = SMatrix::<f64, 2, 2>::new_random();
             let mat_b = mat_r.transpose() * mat_r;
 
             let mat_c = simultaneous_reduction(mat_a, mat_b);
 
             for _ in 0..100 {
-                let v = SVector::<f64, D>::new_random();
+                let v = SVector::<f64, 2>::new_random();
                 let v = v.normalize();
                 let la2 = v.dot(&(mat_a * v));
                 let lb2 = v.dot(&(mat_b * v));
@@ -119,13 +115,29 @@ mod tests {
     }
 
     #[test]
-    fn test_simred_2d() {
-        test_simred::<2>();
-    }
-
-    #[test]
     fn test_simred_3d() {
-        test_simred::<3>();
+        let eps = 1e-8;
+
+        for _ in 0..100 {
+            let mat_r = SMatrix::<f64, 3, 3>::new_random();
+            let mat_a = mat_r.transpose() * mat_r;
+            let mat_r = SMatrix::<f64, 3, 3>::new_random();
+            let mat_b = mat_r.transpose() * mat_r;
+
+            let mat_c = simultaneous_reduction(mat_a, mat_b);
+
+            for _ in 0..100 {
+                let v = SVector::<f64, 3>::new_random();
+                let v = v.normalize();
+                let la2 = v.dot(&(mat_a * v));
+                let lb2 = v.dot(&(mat_b * v));
+                let lc2 = v.dot(&(mat_c * v));
+                assert!(la2 > 0.0);
+                assert!(lb2 > 0.0);
+                assert!(lc2 > (1.0 - eps) * la2);
+                assert!(lc2 > (1.0 - eps) * lb2);
+            }
+        }
     }
 
     #[test]
