tucanos/tucanos

commit 05151a499d3a5a4bba0d7c3754cb4fe3f1aedf6c

Add Cavity::barycenter
We will soon allow to split faces. Spliting and edge or a face is
splitting at barycenter. This is code factorization.

src/cavity.rs
@@ -149,6 +149,26 @@ impl<const D: usize, E: Elem, M: Metric<D>> Cavity<D, E, M> {
         );
     }
 
+    /// Return the coordinate and the metric of the barycenter of the points used
+    /// to generate this cavity (ex: 2 points after using `init_from_edge`)
+    pub fn barycenter(&self) -> (Point<D>, M) {
+        let local_ids = match &self.ctype {
+            CavityType::No => unreachable!(),
+            CavityType::Vertex(x) => std::slice::from_ref(x),
+            CavityType::Edge(x) => x.as_slice(),
+            CavityType::Face(x) => x.as_slice(),
+        };
+        let scale = 1. / local_ids.len() as f64;
+        (
+            local_ids
+                .iter()
+                .map(|&i| self.points[i as usize])
+                .sum::<Point<D>>()
+                * scale,
+            M::interpolate(self.metrics.iter().map(|x| (scale, x))),
+        )
+    }
+
     /// Build the local cavity from a list of elements
     fn compute(&mut self, r: &Remesher<D, E, M>, global_elems: &[Idx], x: CavityType<D>) {
         self.clear();

src/remesher.rs
@@ -701,21 +701,14 @@ impl<const D: usize, E: Elem, M: Metric<D>> Remesher<D, E, M> {
                     let CavityType::Edge(local_edg) = cavity.ctype else {
                         unreachable!()
                     };
-                    let i0 = local_edg[0] as usize;
-                    let i1 = local_edg[1] as usize;
-                    let p0 = &cavity.points[i0];
-                    let p1 = &cavity.points[i1];
-                    let m0 = &cavity.metrics[i0];
-                    let m1 = &cavity.metrics[i1];
-                    let t0 = cavity.tags[i0];
-                    let t1 = cavity.tags[i1];
-
-                    // edge center
-                    let mut new_p = (p0 + p1) * 0.5;
-                    // metric
-                    let new_m = M::interpolate([(0.5, m0), (0.5, m1)].into_iter());
-                    // tag
-                    let tag = self.topo.parent(t0, t1).unwrap();
+                    let (mut edge_center, new_metric) = cavity.barycenter();
+                    let tag = self
+                        .topo
+                        .parent(
+                            cavity.tags[local_edg[0] as usize],
+                            cavity.tags[local_edg[1] as usize],
+                        )
+                        .unwrap();
 
                     // tag < 0 on fixed boundaries
                     if tag.1 < 0 {
@@ -723,11 +716,14 @@ impl<const D: usize, E: Elem, M: Metric<D>> Remesher<D, E, M> {
                     }
                     // projection if needed
                     if tag.0 < E::DIM as Dim {
-                        geom.project(&mut new_p, &tag);
+                        geom.project(&mut edge_center, &tag);
                     }
 
-                    let filled_cavity =
-                        FilledCavity::from_cavity_and_new_vertex(&cavity, &new_p, &new_m);
+                    let filled_cavity = FilledCavity::from_cavity_and_new_vertex(
+                        &cavity,
+                        &edge_center,
+                        &new_metric,
+                    );
 
                     // lower the min quality threshold if the min quality in the cavity increases
                     let q_min = q_min.min(cavity.q_min);
@@ -736,7 +732,7 @@ impl<const D: usize, E: Elem, M: Metric<D>> Remesher<D, E, M> {
                         for i in &cavity.global_elem_ids {
                             self.remove_elem(*i);
                         }
-                        let ip = self.insert_vertex(new_p, &tag, &new_m);
+                        let ip = self.insert_vertex(edge_center, &tag, &new_metric);
                         for face in cavity.faces() {
                             let f = cavity.global_face(&face);
                             assert!(!f.contains_edge(edg));

commit f7bcae04f7513445b9b466ddc51acb20ddfd856b

Bug fix in Cavity::seed_barycenter
* The metric at the barycenter was false
* Regression introduced in 05151a4

src/cavity.rs
@@ -165,7 +165,7 @@ impl<const D: usize, E: Elem, M: Metric<D>> Cavity<D, E, M> {
                 .map(|&i| self.points[i as usize])
                 .sum::<Point<D>>()
                 * scale,
-            M::interpolate(self.metrics.iter().map(|x| (scale, x))),
+            M::interpolate(local_ids.iter().map(|&i| (scale, &self.metrics[i as usize])))
         )
     }
 