5d071022ef2d8e8c1c273aef6434c759cee4d5e6
Jérôme Robert <jeromerobert@gmx.com>
2024-02-26 12:27:50 +0100

    Check that barycenter coordinates are valid

--- src/mesh_interpolate.rs
+++ src/mesh_interpolate.rs
@@ -8,6 +8,9 @@ use crate::{
 
 impl<const D: usize, E: Elem> SimplexMesh<D, E> {
     pub fn interpolate(&self, other: &Self, f: &[f64]) -> Result<Vec<f64>> {
+        self.interpolate_check(other, f, false)
+    }
+    pub fn interpolate_check(&self, other: &Self, f: &[f64], check: bool) -> Result<Vec<f64>> {
         let n_verts = self.n_verts() as usize;
         let n_verts_other = other.n_verts() as usize;
         assert_eq!(f.len() % n_verts, 0);
@@ -21,6 +24,11 @@ impl<const D: usize, E: Elem> SimplexMesh<D, E> {
             let e = self.elem(i_elem);
             let ge = self.gelem(e);
             let x = ge.bcoords(&vert);
+            if check {
+                x.as_slice_f64()
+                    .iter()
+                    .for_each(|c| assert!((-5e-16..=1.).contains(c), "{x:?}"));
+            }
             for j in 0..n_comp {
                 let iter = e.iter().copied().zip(x.as_slice_f64().iter().copied());
                 res.push(iter.fold(0.0, |a, (i, w)| a + f[n_comp * i as usize + j] * w));
@@ -45,14 +53,14 @@ mod tests {
         let f: Vec<f64> = mesh.verts().map(|p| p[0]).collect();
 
         let other = test_mesh_2d().split().split().split().split();
-        let f_other = mesh.interpolate(&other, &f)?;
+        let f_other = mesh.interpolate_check(&other, &f, true)?;
 
         for (a, b) in other.verts().map(|p| p[0]).zip(f_other.iter().copied()) {
             assert!(f64::abs(b - a) < 1e-10);
         }
 
         let other = test_mesh_2d().split();
-        let f_other = mesh.interpolate(&other, &f)?;
+        let f_other = mesh.interpolate_check(&other, &f, true)?;
 
         for (a, b) in other.verts().map(|p| p[0]).zip(f_other.iter().copied()) {
             assert!(f64::abs(b - a) < 1e-10);
@@ -69,14 +77,14 @@ mod tests {
         let f: Vec<f64> = mesh.verts().map(|p| p[0]).collect();
 
         let other = test_mesh_3d().split().split().split().split();
-        let f_other = mesh.interpolate(&other, &f)?;
+        let f_other = mesh.interpolate_check(&other, &f, true)?;
 
         for (a, b) in other.verts().map(|p| p[0]).zip(f_other.iter().copied()) {
             assert!(f64::abs(b - a) < 1e-10);
         }
 
         let other = test_mesh_3d().split();
-        let f_other = mesh.interpolate(&other, &f)?;
+        let f_other = mesh.interpolate_check(&other, &f, true)?;
 
         for (a, b) in other.verts().map(|p| p[0]).zip(f_other.iter().copied()) {
             assert!(f64::abs(b - a) < 1e-10);
