43ada7933393d0605bd0f7291edc6720bd4faee6
Xavier Garnaud <garnaudxavier@gmail.com>
2025-04-18 13:17:01 +0200

    test vtk output from python

--- pytmesh/pytmesh/test_dual.py
+++ pytmesh/pytmesh/test_dual.py
@@ -1,5 +1,7 @@
 import unittest
 import numpy as np
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy # type: ignore
 from . import Mesh2d, Mesh3d, DualType, DualMesh2d, DualMesh3d
 
 class TestMeshes(unittest.TestCase):
@@ -40,6 +42,32 @@ class TestMeshes(unittest.TestCase):
         self.assertEqual(dual_bdy.n_elems(), 4 * ((nx - 1)  +  (ny - 1)))
         self.assertEqual(dual_bdy.n_faces(), 4)
 
+    def test_2d_vtk(self):
+
+        nx = 10
+        ny = 15
+        msh = Mesh2d.rectangle_mesh(np.linspace(0, 1, nx), np.linspace(0, 2, ny))
+        dual = DualMesh2d(msh, DualType.Barth)
+
+        dual.write_vtk("dual_2d.vtu")
+
+        reader = vtk.vtkXMLUnstructuredGridReader()
+        reader.SetFileName("dual_2d.vtu")
+        reader.Update()
+
+        data = reader.GetOutput()
+        self.assertEqual(dual.n_verts(), data.GetNumberOfPoints())
+        self.assertEqual(dual.n_elems(), data.GetNumberOfCells())
+        
+        alg = vtk.vtkCellSizeFilter()
+        alg.SetInputDataObject(data)
+        alg.Update()
+        data = alg.GetOutput()
+        cell_data = data.GetCellData()
+        self.assertEqual(cell_data.GetArrayName(3), "Area")
+        vol = vtk_to_numpy(cell_data.GetArray(3))
+        self.assertAlmostEqual(vol.sum(), 2)
+
     def test_3d(self):
 
         nx = 10
@@ -58,3 +86,29 @@ class TestMeshes(unittest.TestCase):
         self.assertEqual(bdy.n_elems(), 6 * msh.n_faces())
         self.assertEqual(bdy.n_faces(), 2*4 * ((nx - 1) + (ny - 1) + (nz - 1)))
 
+    def test_3d_vtk(self):
+
+        nx = 5
+        ny = 6
+        nz = 7
+        msh = Mesh3d.box_mesh(np.linspace(0, 1, nx), np.linspace(0, 2, ny), np.linspace(0, 3, nz))
+        dual = DualMesh3d(msh, DualType.Median)
+        
+        dual.write_vtk("dual_3d.vtu")
+
+        reader = vtk.vtkXMLUnstructuredGridReader()
+        reader.SetFileName("dual_3d.vtu")
+        reader.Update()
+
+        data = reader.GetOutput()
+        self.assertEqual(dual.n_verts(), data.GetNumberOfPoints())
+        self.assertEqual(dual.n_elems(), data.GetNumberOfCells())
+        
+        alg = vtk.vtkCellSizeFilter()
+        alg.SetInputDataObject(data)
+        alg.Update()
+        data = alg.GetOutput()
+        cell_data = data.GetCellData()
+        self.assertEqual(cell_data.GetArrayName(4), "Volume")
+        vol = vtk_to_numpy(cell_data.GetArray(4))
+        self.assertAlmostEqual(vol.sum(), 6)
