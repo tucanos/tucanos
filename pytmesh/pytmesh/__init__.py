9e5f8f6616bb9036b4f342c34802a0cc10c4bc13
Xavier Garnaud <garnaudxavier@gmail.com>
2025-07-08 13:53:08 +0200

    use ruff in CI

--- pytmesh/pytmesh/__init__.py
+++ pytmesh/pytmesh/__init__.py
@@ -1,11 +1,11 @@
-from .pytmesh import PyMesh2d as Mesh2d
-from .pytmesh import PyBoundaryMesh2d as BoundaryMesh2d
-from .pytmesh import PyMesh3d as Mesh3d
-from .pytmesh import PyBoundaryMesh3d as PyBoundaryMesh3d
-from .pytmesh import PyDualType as DualType
-from .pytmesh import PyDualMesh2d as DualMesh2d
-from .pytmesh import PyDualMesh3d as DualMesh3d
-from .pytmesh import PyPolyMeshType as PolyMeshType
-from .pytmesh import PyPolyMesh2d as PolyMesh2d
-from .pytmesh import PyPolyMesh3d as PolyMesh3d
-from .pytmesh import PyExtrudedMesh2d as ExtrudedMesh2d
+from .pytmesh import PyMesh2d as Mesh2d  # noqa: F401
+from .pytmesh import PyBoundaryMesh2d as BoundaryMesh2d  # noqa: F401
+from .pytmesh import PyMesh3d as Mesh3d  # noqa: F401
+from .pytmesh import PyBoundaryMesh3d as PyBoundaryMesh3d  # noqa: F401
+from .pytmesh import PyDualType as DualType  # noqa: F401
+from .pytmesh import PyDualMesh2d as DualMesh2d  # noqa: F401
+from .pytmesh import PyDualMesh3d as DualMesh3d  # noqa: F401
+from .pytmesh import PyPolyMeshType as PolyMeshType  # noqa: F401
+from .pytmesh import PyPolyMesh2d as PolyMesh2d  # noqa: F401
+from .pytmesh import PyPolyMesh3d as PolyMesh3d  # noqa: F401
+from .pytmesh import PyExtrudedMesh2d as ExtrudedMesh2d  # noqa: F401
