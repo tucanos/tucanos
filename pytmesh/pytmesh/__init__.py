20e77e823ca51a7fad1c558087a4a6b8771c0fee
Xavier Garnaud <garnaudxavier@gmail.com>
2025-07-25 15:36:46 +0200

    move cgns_io to tmesh

--- pytmesh/pytmesh/__init__.py
+++ pytmesh/pytmesh/__init__.py
@@ -1,7 +1,7 @@
 from .pytmesh import PyMesh2d as Mesh2d  # noqa: F401
 from .pytmesh import PyBoundaryMesh2d as BoundaryMesh2d  # noqa: F401
 from .pytmesh import PyMesh3d as Mesh3d  # noqa: F401
-from .pytmesh import PyBoundaryMesh3d as PyBoundaryMesh3d  # noqa: F401
+from .pytmesh import PyBoundaryMesh3d as BoundaryMesh3d  # noqa: F401
 from .pytmesh import PyDualType as DualType  # noqa: F401
 from .pytmesh import PyDualMesh2d as DualMesh2d  # noqa: F401
 from .pytmesh import PyDualMesh3d as DualMesh3d  # noqa: F401
@@ -10,3 +10,4 @@ from .pytmesh import PyPolyMesh2d as PolyMesh2d  # noqa: F401
 from .pytmesh import PyPolyMesh3d as PolyMesh3d  # noqa: F401
 from .pytmesh import PyExtrudedMesh2d as ExtrudedMesh2d  # noqa: F401
 from .pytmesh import HAVE_METIS  # noqa: F401
+from .cgns_io import load_cgns, write_cgns  # noqa: F401
