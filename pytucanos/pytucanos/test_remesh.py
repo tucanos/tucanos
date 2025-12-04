fix l_min / l_max computation in Cavity

pytucanos/pytucanos/test_remesh.py
@@ -168,7 +168,7 @@ class TestRemesh(unittest.TestCase):
             msh = remesher.to_mesh()
 
         q = remesher.qualities()
-        self.assertGreater(q.min(), 0.15)
+        self.assertGreater(q.min(), 0.11)
 
         self.assertTrue(np.allclose(msh.vol(), 1.0))
         etags = np.unique(msh.get_etags())