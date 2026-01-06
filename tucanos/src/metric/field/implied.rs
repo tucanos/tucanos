2c28fd5db382a6c0c60b849d2e3b58cc33dd1401
Jérôme Robert <jeromerobert@gmx.com>
2026-01-06 10:25:51 +0100

    Add implied_metric for GTriangle<3>

--- tucanos/src/metric/field/implied.rs
+++ tucanos/src/metric/field/implied.rs
@@ -1,7 +1,10 @@
 use crate::metric::{AnisoMetric, AnisoMetric2d, AnisoMetric3d, Metric, MetricField};
 use nalgebra::{Matrix2, Matrix3};
 use rayon::prelude::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
-use tmesh::mesh::{GTetrahedron, GTriangle, Mesh, Simplex};
+use tmesh::{
+    Vert2d,
+    mesh::{GSimplex, GTetrahedron, GTriangle, Mesh, Simplex},
+};
 
 /// Trait of geometric simplices that can compute an implied metric
 pub trait ImpliedMetric<T> {
@@ -90,14 +93,50 @@ impl ImpliedMetric<AnisoMetric3d> for GTetrahedron<3> {
     }
 }
 
+impl ImpliedMetric<AnisoMetric3d> for GTriangle<3> {
+    /// Computes the implied metric.
+    ///
+    /// This extends the triangle's 2D metric into the normal direction:
+    /// - in the plane of the triangle it is given by the 2d implied metric
+    /// - in the normal direction, the size is the square root of the product of the
+    ///   sizes in the triangle plane
+    fn implied_metric(&self) -> AnisoMetric3d {
+        // Build a local basis
+        let (v0, v1) = (self.edge(0).as_vec(), -self.edge(2).as_vec());
+        let v0n = v0.norm();
+        let (e0, n) = (v0 / v0n, v0.cross(&v1).normalize());
+        let e1 = n.cross(&e0);
+
+        // Compute the 2d implied metric
+        let m2 = GTriangle::new(
+            &Vert2d::zeros(),
+            &Vert2d::new(v0n, 0.0),
+            &Vert2d::new(v1.dot(&e0), v1.dot(&e1)),
+        )
+        .implied_metric()
+        .as_mat();
+
+        // Compute the 3d implied metric in the local basis
+        let mut m3 = m2.fixed_resize(0.0);
+        m3[(2, 2)] = m2.determinant().sqrt();
+
+        // Compute the 3d implied metric in the global basis
+        let p = Matrix3::from_columns(&[e0, e1, n]);
+        AnisoMetric3d::from_mat(p * m3 * p.transpose())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{
         mesh::test_meshes::test_mesh_3d,
-        metric::{Metric, MetricField},
+        metric::{AnisoMetric, ImpliedMetric, Metric, MetricField},
     };
-    use nalgebra::Matrix3;
-    use tmesh::mesh::Mesh;
+    use nalgebra::{Matrix3, Rotation3};
+    use tmesh::{
+        Vert2d, Vert3d, assert_delta,
+        mesh::{GTriangle, Mesh},
+    };
 
     #[test]
     fn test_implied_metric() {
@@ -137,5 +176,36 @@ mod tests {
             assert!(s[1] > 0.33 * h2 / 8. && s[1] < 3. * h2 / 8.);
             assert!(s[2] > 0.33 * h0 / 8. && s[2] < 3. * h0 / 8.);
         }
+    }
+    #[test]
+    fn test_implied_metric_triangle_3d() {
+        let (p0_2d, p1_2d, p2_2d) = (
+            Vert2d::zeros(),
+            Vert2d::new(0.1, 0.2),
+            Vert2d::new(0.3, 0.4),
+        );
+        let m2d = GTriangle::new(&p0_2d, &p1_2d, &p2_2d).implied_metric();
+
+        let r = Rotation3::from_euler_angles(0.1, 0.2, 0.3);
+        let (p0, p1, p2) = (
+            r * Vert3d::zeros(),
+            r * p1_2d.fixed_resize(0.0),
+            r * p2_2d.fixed_resize(0.0),
+        );
+        let m3d = GTriangle::new(&p0, &p1, &p2).implied_metric();
+
+        // Check length in triangle plane
+        let (e, f) = (0.5, 0.6);
+        let l2d = m2d.length(&(e * p0_2d + f * p1_2d));
+        let l3d = m3d.length(&(e * p0 + f * p1));
+        assert_delta!(l2d, l3d, 1e-8);
+
+        // Check length along normal
+        let n = p1.cross(&p2).normalize();
+        assert_delta!(
+            m3d.length(&n),
+            m2d.as_mat().determinant().sqrt().sqrt(),
+            1e-8
+        );
     }
 }
